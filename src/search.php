22ed8906239baa39fd5f17595b7631a8fd620475
neobutter <neobutter@me.com>
2017-11-10 02:31:07 +0900

    Fixed

--- src/search.php
+++ src/search.php
@@ -8,14 +8,13 @@ class SynoDLMSearchNyaa {
 	public function prepare($curl, $query) {
 		$url = $this->qurl . urlencode($query);
 		curl_setopt($curl, CURLOPT_URL, $url);
-		//curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 	}
 
 	public function parse($plugin, $response) {
 		$response = preg_replace("/nyaa:/i", "", $response);
-		$response = preg_replace("/seeders/i", "seeds", $response);
-		$response = preg_replace("/leechers/i", "leechs", $response);
-		$response = preg_replace("/infoHash/i", "hash", $response);
+		//$response = preg_replace("/seeders/i", "seeds", $response);
+		//$response = preg_replace("/leechers/i", "leechs", $response);
+		//$response = preg_replace("/infoHash/i", "hash", $response);
 		if ($plugin == null) {
 			return $response;
 		} else {

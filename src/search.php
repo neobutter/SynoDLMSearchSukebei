a97ed165e0516e58760ff6363c7667a1eac2afaa
박창용 <cypark85@interpark.com>
2017-11-10 13:49:34 +0900

    Fixed

--- src/search.php
+++ src/search.php
@@ -7,7 +7,8 @@ class SynoDLMSearchSukebei {
 
 	public function prepare($curl, $query) {
 		$url = $this->qurl . urlencode($query);
-		curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     }
     
     private function size_format($sizestr) {
@@ -27,7 +28,7 @@ class SynoDLMSearchSukebei {
 
 	public function parse($plugin, $response) {
         $response = preg_replace("/nyaa:/i", "", $response);
-        $response = preg_replace_callback('/(<size[^>]*>)(.*?)(<\/size>)/i', "size_format", $response);
+        //$response = preg_replace_callback('/(<size[^>]*>)(.*?)(<\/size>)/i', "size_format", $response);
 		$response = preg_replace("/seeders/i", "seeds", $response);
 		$response = preg_replace("/leechers/i", "leechs", $response);
 		$response = preg_replace("/infoHash/i", "hash", $response);

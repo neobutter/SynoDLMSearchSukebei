d4acd3966e3b2fb3fc6f786cc9abf4527e42912a
박창용 <cypark85@interpark.com>
2017-11-10 14:06:31 +0900

    fixed

--- src/search.php
+++ src/search.php
@@ -10,25 +10,9 @@ class SynoDLMSearchSukebei {
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     }
-    
-    private function size_format($sizestr) {
-		$size_map = array(
-			"KiB" => 1024,
-			"MiB" => 1048576,
-			"GiB" => 1073741824,
-        );
-		foreach ($size_map as $n => $mux) {
-			if( strstr($sizestr,$n) ){
-				$sizestr=floatval($sizestr)*$mux;
-				break;
-			}
-		}
-		return $sizestr;
-	}
 
 	public function parse($plugin, $response) {
         $response = preg_replace("/nyaa:/i", "", $response);
-        //$response = preg_replace_callback('/(<size[^>]*>)(.*?)(<\/size>)/i', "size_format", $response);
 		$response = preg_replace("/seeders/i", "seeds", $response);
 		$response = preg_replace("/leechers/i", "leechs", $response);
 		$response = preg_replace("/infoHash/i", "hash", $response);

084f46d96dcd146c09cc02882447141b9d3a68cd
neobutter <neobutter@me.com>
2017-11-18 17:27:57 +0900

    Fixed

--- src/search.php
+++ src/search.php
@@ -8,15 +8,12 @@ class SynoDLMSearchSukebei {
 	public function prepare($curl, $query) {
 		$url = $this->qurl . urlencode($query);
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     }
 
 	public function parse($plugin, $response) {
         $response = preg_replace("/nyaa:/i", "", $response);
-        $response = preg_replace("/]]>/i", "]]><br />Seeders: 1", $response);
-        $response = preg_replace("/]]>/i", "]]><br />leechers: 1", $response);
-		//$response = preg_replace("/seeders/i", "seeds", $response);
-		//$response = preg_replace("/leechers/i", "leechs", $response);
+        $response = preg_replace("/]]>/i", "| Ratio: 1 seeds, 1 leechers", $response);
+        //$response = preg_replace("/]]>/i", "leechers: 1", $response);
         $response = preg_replace("/infoHash/i", "hash", $response);
         if ($plugin == null) {
             return $response;

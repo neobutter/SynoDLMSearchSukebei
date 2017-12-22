dcc0647c16359fe3ac2aea194948867c6fbf211a
Changyong Park <neobutter@gmail.com>
2017-12-22 15:01:03 +0900

    Update search.php

--- src/search.php
+++ src/search.php
@@ -11,7 +11,7 @@ class SynoDLMSearchSukebei {
     }
 
 	public function parse($plugin, $response) {
-	$response = preg_replace_callback("/([0-9]+)(?=<\/nyaa:seeders>).+([0-9]+)(?=<\/nyaa:leechers>).+([0-9a-zA-Z]+]]>)/sU", create_function('$matches', 'return str_replace($matches[3], sprintf("Seeds : %s | Leechs : %s | %s", $matches[1], $matches[2], $matches[3]), $matches[0]);'), $response);
+	$response = preg_replace_callback("/([0-9]+)(?=<\/nyaa:seeders>).+([0-9]+)(?=<\/nyaa:leechers>).+([0-9a-zA-Z]+]]>)/sU", create_function('$matches', 'return str_replace($matches[3], sprintf("Ratio : %s seeds %s leechers | %s", $matches[1], $matches[2], $matches[3]), $matches[0]);'), $response);
         //$response = preg_replace("/nyaa:/i", "", $response);
         //$response = preg_replace("/infoHash/i", "hash", $response);
         if ($plugin == null) {

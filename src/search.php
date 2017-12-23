693cf5ada6f0f12e555709178b0e19acf6a64feb
neobutter <neobutter@me.com>
2017-12-23 13:58:37 +0900

    Fixed

--- src/search.php
+++ src/search.php
@@ -11,8 +11,8 @@ class SynoDLMSearchSukebei {
     }
 
 	public function parse($plugin, $response) {
-	$response = preg_replace_callback("/([0-9]+)(?=<\/nyaa:seeders>).+([0-9]+)(?=<\/nyaa:leechers>).+([0-9a-zA-Z]+]]>)/sU", create_function('$matches', 'return str_replace($matches[3], sprintf("Ratio : %s seeds %s leechers | %s", $matches[1], $matches[2], $matches[3]), $matches[0]);'), $response);
-        //$response = preg_replace("/nyaa:/i", "", $response);
+	    $response = preg_replace_callback("/([0-9]+)(?=<\/nyaa:seeders>).+([0-9]+)(?=<\/nyaa:leechers>).+([0-9a-zA-Z]+]]>)/sU", create_function('$matches', 'return str_replace($matches[3], sprintf("Ratio : %s seeders %s leechers | %s", $matches[1], $matches[2], $matches[3]), $matches[0]);'), $response);
+        $response = preg_replace("/nyaa:/i", "", $response);
         //$response = preg_replace("/infoHash/i", "hash", $response);
         if ($plugin == null) {
             return $response;
